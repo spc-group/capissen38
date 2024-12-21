b531cce5ab9fb49877f062f704ee4b4a86e03391
Mark Wolfman <canismarko@gmail.com>
2024-12-21 17:15:38 -0600

    Added overlay plugins to the haven area detector mixin.

--- src/haven/devices/detectors/area_detectors.py
+++ src/haven/devices/detectors/area_detectors.py
@@ -1,11 +1,40 @@
 from pathlib import Path
 
+from ophyd_async.core import PathProvider, DeviceVector, SubsetEnum
 from ophyd_async.core import UUIDFilenameProvider, YMDPathProvider
+from ophyd_async.epics.adcore._core_io import NDPluginBaseIO
+from ophyd_async.epics.core import epics_signal_rw_rbv
 
 from ..._iconfig import load_config
 
 
-class HavenDetector: ...
+class OverlayShape(SubsetEnum):
+    CROSS = "Cross"
+    RECTANGLE = "Rectangle"
+    ELLIPSE = "Ellipse"
+    TEXT = "Text"
+
+
+class OverlayPlugin(NDPluginBaseIO):
+    def __init__(self, prefix: str, name: str = "") -> None:
+        self.shape = epics_signal_rw_rbv(OverlayShape, f"{prefix}Shape")
+        self.center_x = epics_signal_rw_rbv(int, f"{prefix}CenterX")
+        self.center_y = epics_signal_rw_rbv(int, f"{prefix}CenterY")
+        self.size_x = epics_signal_rw_rbv(int, f"{prefix}SizeX")
+        self.size_y = epics_signal_rw_rbv(int, f"{prefix}SizeY")
+        super().__init__(prefix=prefix, name=name)
+
+
+class HavenDetector:
+    def __init__(self, *args, prefix: str, path_provider: PathProvider | None = None, **kwargs):
+        # Add additional non-data plugins
+        self.overlays = DeviceVector({
+            idx: OverlayPlugin(f"{prefix}Over1:{idx+1}:") for idx in range(8)
+        })
+        # Determine a default path provider to use
+        if path_provider is None:
+            path_provider = default_path_provider()
+        super().__init__(*args, prefix=prefix, path_provider=path_provider, **kwargs)
 
 
 def default_path_provider(path: Path = None, config=None):
--- src/haven/tests/test_sim_detector.py
+++ src/haven/tests/test_sim_detector.py
@@ -3,25 +3,26 @@ import pytest
 from ophyd import Signal
 from ophyd.sim import instantiate_fake_device
 
-from haven.devices import SimDetectorThreaded
+from haven.devices import SimDetectorThreaded, SimDetector
 
 
 @pytest.fixture()
-def sim_det(sim_registry):
-    sim_det = instantiate_fake_device(
-        SimDetectorThreaded,
-        prefix="255idSimDet:",
-        name="sim_det"
-    )
-    return sim_det
+async def sim_det(sim_registry):
+    det = SimDetector(name="sim_det", prefix="255idSimDet:")
+    await det.connect(mock=True)
+    return det
 
 
 def test_signals(sim_det):
-    assert sim_det.drv.prefix == "255idSimDet:cam1:"
-    assert type(sim_det.drv).acquire.suffix == "Acquire"
-    assert type(sim_det.drv).acquire_time.suffix == "AcquireTime"
-    assert type(sim_det.drv).detector_state.suffix == "DetectorState_RBV"
-    assert type(sim_det.drv).acquire_busy.suffix == "AcquireBusy"
-    assert type(sim_det.drv).gain.suffix == "Gain"
-    assert type(sim_det.hdf).file_path.suffix == "FilePath"
-    assert type(sim_det.hdf).file_name.suffix == "FileName"
+    assert sim_det.drv.acquire.source == "mock+ca://255idSimDet:cam1:Acquire_RBV"
+    assert sim_det.drv.acquire_time.source == "mock+ca://255idSimDet:cam1:AcquireTime_RBV"
+    assert sim_det.drv.detector_state.source == "mock+ca://255idSimDet:cam1:DetectorState_RBV"
+    # assert sim_det.drv.gain.source == "mock+ca://255idSimDet:cam1:Gain"
+    assert sim_det.hdf.file_path.source == "mock+ca://255idSimDet:HDF1:FilePath_RBV"
+    assert sim_det.hdf.file_name.source == "mock+ca://255idSimDet:HDF1:FileName_RBV"
+    # Overlays for showing annotations on-screen only
+    assert sim_det.overlays[0].shape.source == "mock+ca://255idSimDet:Over1:1:Shape_RBV"
+    assert sim_det.overlays[0].center_x.source == "mock+ca://255idSimDet:Over1:1:CenterX_RBV"
+    assert sim_det.overlays[0].center_y.source == "mock+ca://255idSimDet:Over1:1:CenterY_RBV"
+    assert sim_det.overlays[0].size_x.source == "mock+ca://255idSimDet:Over1:1:SizeX_RBV"
+    assert sim_det.overlays[0].size_y.source == "mock+ca://255idSimDet:Over1:1:SizeY_RBV"
