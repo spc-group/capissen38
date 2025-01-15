1fab41f9abdc8e734b35981e9a5b601ca3b3ac03
Mark Wolfman <canismarko@gmail.com>
2025-01-15 09:13:30 -0600

    Better error handling/reporting, and dry-run option.

--- migrate_mongo_to_sql.py
+++ migrate_mongo_to_sql.py
@@ -1,13 +1,14 @@
 #!/bin/env python
-import argparse
 import logging
+import sys
+import argparse
 from collections import OrderedDict
 
-from bluesky.callbacks.tiled_writer import TiledWriter
 from pymongo import MongoClient
 from pymongo.errors import CursorNotFound
-from tiled.client import from_uri
 from tqdm import tqdm
+from tiled.client import from_uri
+from bluesky.callbacks.tiled_writer import TiledWriter
 
 log = logging.getLogger(__name__)
 
@@ -15,18 +16,13 @@ log = logging.getLogger(__name__)
 def main():
     logging.basicConfig(level=logging.WARNING)
     parser = argparse.ArgumentParser(
-        prog="mongo2sql",
-        description="Migrate mongo database to SQL database one document at a time",
+        prog='mongo2sql',
+        description='Migrate mongo database to SQL database one document at a time',
     )
-    parser.add_argument("mongo_uri")
+    parser.add_argument('mongo_uri')
     parser.add_argument("tiled_uri")
     parser.add_argument("--api-key", help="Tiled API key")
-    parser.add_argument(
-        "-n",
-        "--dry-run",
-        help="Do not actually commit documents to the new database.",
-        action="store_true",
-    )
+    parser.add_argument("-n", "--dry-run", help="Do not actually commit documents to the new database.", action="store_true")
     args = parser.parse_args()
     # Prepare the database objects
     mongo_client = MongoClient(args.mongo_uri)
@@ -36,34 +32,38 @@ def main():
     tiled_writer = TiledWriter(tiled_client[tiled_catalog])
     # Write the documents
     # ['run_start', 'event', 'run_stop', 'event_descriptor', 'resource', 'datum']
-    collections = OrderedDict(
-        {
-            "run_start": "start",
-            "event_descriptor": "descriptor",
-            "resource": "resource",
-            "event": "event",
-            "datum": "datum",
-            "run_stop": "stop",
-        }
-    )
+    collections = OrderedDict({
+        "run_start": "start",
+        "event_descriptor": "descriptor",
+        "resource": "resource",
+        "event": "event",
+        "datum": "datum",
+        "run_stop": "stop",
+    })
     for coll_name, doc_name in collections.items():
         coll = mongo_db.get_collection(coll_name)
-        docs = coll.find()
+        docs =  coll.find()
         prog = tqdm(total=coll.estimated_document_count(), desc=coll_name, unit="doc")
+        num_failures = 0
         while True:
             try:
                 doc = next(docs)
             except StopIteration:
                 break
             except CursorNotFound as exc:
-                log.exception(exc)
+                num_failures += 1
             finally:
                 prog.update()
             doc.pop("_id", None)
             # Commit the document to Tiled
             if not args.dry_run:
-                tiled_writer(doc_name, doc)
-
+                try:
+                    tiled_writer(doc_name, doc)
+                except Exception as exc:
+                    num_failures += 1
+                    continue
+        # Reporting
+        print(f"{coll_name} failed: {num_failures}")
 
 if __name__ == "__main__":
     main()
